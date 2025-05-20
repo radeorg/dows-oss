8a6c009b1ef1b73ce259b76e315324545ac1ac7d
lait <lait.zhang@gmail.com>
2025-05-20 18:19:56 +0800

    init

---
+++ src/main/java/org/dows/oss/config/UploadAutoConfiguration.java
@@ -0,0 +1,9 @@
+package org.dows.oss.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.dows.oss")
+public class UploadAutoConfiguration {
+}
