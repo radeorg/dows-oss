8acccb7864959d04b5a8051fbc3a462e5d864f1c
lait <lait.zhang@gmail.com>
2025-06-15 12:38:03 +0800

    feat(oss-handler): 添加 MdTransformTrigger 触发器
    - 新增 MdTransformTrigger 类实现 FileTrigger 接口
    - 用于处理文件转换相关的触发事件

---
+++ oss-handler/src/main/java/org/dows/oss/trigger/MdTransformTrigger.java
@@ -0,0 +1,21 @@
+package org.dows.oss.trigger;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.dows.oss.entity.OssTriggerEntity;
+import org.springframework.stereotype.Component;
+
+/**
+ * 文件转换触发器
+ */
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class MdTransformTrigger implements FileTrigger {
+
+    @Override
+    public void trigger(Object object, OssTriggerEntity ossTriggerEntity) {
+
+    }
+}
